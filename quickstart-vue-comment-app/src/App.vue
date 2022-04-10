f40829df07af7ae940067e73bcc4d2b8d6f0b9db
wenner <nelsondiasdemedeiros@gmail.com>
2022-04-10 20:36:28 -0300

    Add project comment app

---
+++ quickstart-vue-comment-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+  import Comments from './components/Comments/index.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Comments
+    }
+  }
+</script>
